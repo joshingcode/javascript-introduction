20737668ce5cd82de8cf7ccd2196ddaccf005333
Josh Sease <joshsease96@gmail.com>
2022-04-12 17:16:11 -0400

    medium done

---
+++ medium/js/main.js
@@ -0,0 +1,14 @@
+
+
+let user = prompt('how are you')
+let goodFeel= user.toUpperCase()
+let badFeel= user.toLowerCase()
+
+if (user === goodFeel){
+    console.log('WHY YOU SCREAM AT ME')
+     
+}else if( user === badFeel) {
+    console.log("speak up i can't hear you")
+}else{
+    console.log("what was said??")
+}
